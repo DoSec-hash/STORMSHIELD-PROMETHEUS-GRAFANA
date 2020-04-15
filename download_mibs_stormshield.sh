360fac89efca9d554b0db7497bb609d6c42803d4
DoSec-hash <62763717+DoSec-hash@users.noreply.github.com>
2020-04-15 20:59:18 +0200

    Create download_mibs_stormshield.sh

---
+++ download_mibs_stormshield.sh
@@ -0,0 +1,52 @@
+#Stormshield-SMI-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-SMI-MIB.txt
+#Stormshield-ALARM-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-ALARM-MIB.txt
+#Stormshield-HA-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-HA-MIB-1.txt
+#Stormshield-POLICY-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-POLICY-MIB.txt
+#Stormshield-AUTHUSERS-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-AUTHUSERS-MIB.txt
+#Stormshield-ROUTE-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-ROUTE-MIB.txt
+#Stormshield-HOSTS-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-HOSTS-MIB.txt
+#STORMSHIELD-PROPERTY-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-PROPERTY-MIB.txt
+#Stormshield-SYSTEM-MONITOR-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-SYSTEM-MONITOR-MIB-1.txt
+#Stormshield-AUTOUPDATE-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-AUTOUPDATE-MIB.txt
+#Stormshield-IF-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-IF-MIB.txt
+#Stormshield-SERVICES-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-SERVICES-MIB.txt
+#Stormshield-VPNSA-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-VPNSA-MIB.txt
+#Stormshield-IPSEC-STATS-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-IPSEC-STATS-MIB.txt
+#Stormshield-ASQ-STATS-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-ASQ-STATS-MIB.txt
+#Stormshield-DISMAN-EVENT-MIB
+wget https://www.stormshield.com/wp-content/uploads/DISMAN-EVENT-MIB.txt
+#Stormshield-RFC1155-SMI
+wget https://www.stormshield.com/wp-content/uploads/RFC1155-SMI.txt
+#Stormshield-RFC1213-MIB
+wget https://www.stormshield.com/wp-content/uploads/RFC1213-MIB.txt
+#Stormshield-SNMP-FRAMEWORK-MIB
+wget https://www.stormshield.com/wp-content/uploads/SNMP-FRAMEWORK-MIB.txt
+#Stormshield-SNMP-TARGET-MIB
+wget https://www.stormshield.com/wp-content/uploads/SNMP-TARGET-MIB.txt
+#Stormshield-SNMPV2-TC
+wget https://www.stormshield.com/wp-content/uploads/SNMPv2-TC-1.txt
+#Stormshield-SNMPV2-SMI
+wget https://www.stormshield.com/wp-content/uploads/SNMPv2-SMI-1.txt
+#Stormshield-SNMPV2-MIB
+wget https://www.stormshield.com/wp-content/uploads/SNMPv2-MIB.txt
+#Stormshield-UCD-SNMP-MIB
+wget https://www.stormshield.com/wp-content/uploads/UCD-SNMP-MIB.txt
+#Stormshield-QOS-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-QOS-MIB.txt
+#Stormshield-HEALTH-MONITOR-MIB
+wget https://www.stormshield.com/wp-content/uploads/STORMSHIELD-HEALTH-MONITOR-MIB.txt
